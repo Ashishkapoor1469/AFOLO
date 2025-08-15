2be7f36a577e9b2812f9cdc4445cf0445b2042a2
Ashish <kapoorashish713@gmail.com>
2025-08-15 14:39:34 +0530

    overflow problem solve

--- src/components/github-projects.tsx
+++ src/components/github-projects.tsx
@@ -113,6 +113,7 @@ export function GithubProjects() {
         </div>
 
         {/* Projects */}
+         <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {loading
             ? Array.from({ length: 3 }).map((_, index) => (
                 <Card
